e3cfa3ad3b851b39785f3934d130b5c9559518cc
Naoaki Sato <nsato@c6r3s8.42tokyo.jp>
2026-05-13 15:18:20 +0900

    update: cli_arg
    エラーハンドリングを更新。pydanticの強制。

--- src/cli_arg.py
+++ src/cli_arg.py
@@ -1,24 +1,39 @@
 import sys
 import argparse
-from pydantic import BaseModel, Field
+from pathlib import Path
+from pydantic import BaseModel, Field, field_validator, ValidationError
 
 
 class CLIConfig(BaseModel):
-    function_definition: str = Field(
-        default="data/input/functions_definition.json",
+    """
+        Pathを使用しWindows、Mac/Linuxのパス形式を補完。
+    """
+    function_definition: Path = Field(
+        default=Path("data/input/functions_definition.json"),
         description="Path to the function definitions JSON file."
     )
-    input: str = Field(
-        default="data/input/function_calling_tests.json",
+    input: Path = Field(
+        default=Path("data/input/function_calling_tests.json"),
         description="Path to the input prompts JSON file"
     )
-    output: str = Field(
-        default="data/output/function_calling_results.json",
+    output: Path = Field(
+        default=Path("data/output/function_calling_results.json"),
         description="Path to the output results JSON file"
     )
 
+    # ユーザーがどのフラグにどんな文字列を入れても必ず.jsonで終わるかチェック
+    @field_validator("*")
+    def check_json_extension(cls, v: Path) -> Path:
+        if v.suffix.lower() != '.json':
+            raise ValueError(f"File must have a '.json' extension: {v}")
+        return v
+
 
 def parse_arguments() -> CLIConfig:
+    """
+        aragparseは自動でSystemExit、
+        その他のパースはpydanticでバリデーションチェック
+    """
     parser = argparse.ArgumentParser(
         description="Introduction to function calling in LLMs"
     )
@@ -26,34 +41,36 @@ def parse_arguments() -> CLIConfig:
     parser.add_argument(
         "-f", "--functions_definition",
         type=str,
-        default="data/input/functions_definition.json",
         help="Path to the function definitions JSON file."
     )
 
     parser.add_argument(
         "-i", "--input",
         type=str,
-        default="data/input/function_calling_tests.json",
         help="Path to the input prompts JSON file"
     )
     parser.add_argument(
         "-o", "--output",
         type=str,
-        default="data/output/function_calling_results.json",
         help="Path to the output results JSON file"
     )
 
     try:
-        # 解析
+        # 解析、不正な引数は自動でSystemExitが呼ばれhelpが出る
         args = parser.parse_args()
+
+        # argparseでNoneになったものを除外して辞書化
+        kwargs = {k: v for k, v in vars(args).items() if v is not None}
+
         # 型検証、安全なデータモデルの生成
-        return CLIConfig(
-            function_definition=args.functions_definition,
-            input=args.input,
-            output=args.output
-        )
+        return CLIConfig(**kwargs)
+
+    except ValidationError as e:
+        print("[\33[31mCLI Error\33[0m]: Argument validation failed.\n"
+              f"{e}", file=sys.stderr)
+        sys.exit(1)
 
     except Exception as e:
-        print(f"[\33[31mCLI Error\33[0m]: Argument parsing failed."
+        print(f"[\33[31mCLI Error\33[0m]: Unexpected error during parsing: \n"
               f"{e}", file=sys.stderr)
-        exit(1)
+        sys.exit(1)
